e62ba1aae917d72c8301273fa4e768f42bc3c86c
Akhil Indurti <aindurti@gmail.com>
2023-06-09 22:38:52 -0700

    initialize dune template

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "yoshi";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    let supportedSystems = [
+      "aarch64-linux"
+      "i686-linux"
+      "x86_64-linux"
+    ]; in
+    flake-utils.lib.eachSystem supportedSystems (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [];
+        };
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            ocaml
+            dune_3
+            ocamlformat
+            ocamlPackages.findlib
+            ocamlPackages.utop
+            ocamlPackages.odoc
+          ];
+        };
+      });
+}
